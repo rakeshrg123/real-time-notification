cb0f4ed23421a7ef6b2d150277387a166edf1749
rakeshrg123 <88580954+rakeshrg123@users.noreply.github.com>
2025-01-18 13:52:54 +0530

    Update socket.js

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000",{
+const socket = io("https://real-time-notification-backend-ilqa.onrender.com",{
     transports: ["websocket", "polling"], // Ensure both protocols are enabled
   }); 
 
